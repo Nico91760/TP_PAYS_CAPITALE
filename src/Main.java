d539e8bd4749e16c8859f2b5c140ab27e6b98015
Nico <nicopalam@hotmail.fr>
2018-09-19 09:58:14 +0200

    TP_PAYS_CAPITALE
    Dernier test après fini

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	private static Scanner clavier;
 	
-	//BLABLAAAAAAAAAAAAAAAAAAAAAAAAAA
+	//BLABLAAAAAAAAAAAAAAAAAAAAAAAAAA TEST
 	
 	public static void main(String[] args) {
 		
